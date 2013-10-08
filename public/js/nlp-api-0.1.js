7832c40d5c9b8dbd0fd173ab0c31ccc69b5c467e
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-07 22:24:17 -0400

    Got rid of charachter limit, much better

--- public/js/nlp-api-0.1.js
+++ public/js/nlp-api-0.1.js
@@ -1,56 +1,64 @@
 var nlp = {};
 nlp.getParse = function(text, callback)
 {
-	if (text.length >250)
-	{
-	return false; //"ERROR: input length greater than 250 characters. Please parse one sentence at a time";
-	}
-	else
-	{
-	var url = 'http://nlp.naturalparsing.com/api/parse?input=?format=json&jsoncallback='+encodeURIComponent(callback)+'&input=' + encodeURIComponent(text) +"&version=0.1&options=sentence";
-	var script = document.createElement("script");
-	script.setAttribute("async", "true");
-	script.setAttribute("src", url);
-	script.setAttribute("type", "text/javascript");
+  if (text.length > 250)
+  {
+    // If more than 250 be really stupid and call everything a noun... 
+    var words = text.split(" ");
+    var obj = {words:[words.length]};
+    for (var i = 0; i < words.length; i++) {
+      obj.words[i] = {value:words[i], tag:"NN"};
+    }
+    callback(obj);
+    // return false; //"ERROR: input length greater than 250 characters. Please parse one sentence at a time";
+  }
+  else
+  {
+    var url = 'http://nlp.naturalparsing.com/api/parse?input=?format=json&jsoncallback='+encodeURIComponent(callback)+'&input=' + encodeURIComponent(text) +"&version=0.1&options=sentence";
+    var script = document.createElement("script");
+    script.setAttribute("async", "true");
+    script.setAttribute("src", url);
+    script.setAttribute("type", "text/javascript");
 	
-	document.body.appendChild(script);
-	return true;
-	}
+    document.body.appendChild(script);
+    return true;
+  }
 };
 
 nlp.getParsedTree = function(text, callback)
 {
-	if (text.length >250)
-	{
-	return false; //"ERROR: input length greater than 250 characters. Please parse one sentence at a time";
-	}
-	else
-	{
-	var url = 'http://nlp.naturalparsing.com/api/parse?input=?format=json&jsoncallback='+encodeURIComponent(callback)+'&input=' + encodeURIComponent(text) +"&version=0.1&options=tree";
-	var script = document.createElement("script");
-	script.setAttribute("async", "true");
-	script.setAttribute("src", url);
-	script.setAttribute("type", "text/javascript");
+  if (text.length >250)
+  {
+
+    return false; //"ERROR: input length greater than 250 characters. Please parse one sentence at a time";
+  }
+  else
+  {
+    var url = 'http://nlp.naturalparsing.com/api/parse?input=?format=json&jsoncallback='+encodeURIComponent(callback)+'&input=' + encodeURIComponent(text) +"&version=0.1&options=tree";
+    var script = document.createElement("script");
+    script.setAttribute("async", "true");
+    script.setAttribute("src", url);
+    script.setAttribute("type", "text/javascript");
 	
-	document.body.appendChild(script);
-	return true;
-	}
+    document.body.appendChild(script);
+    return true;
+  }
 };
 
 nlp.JSONtoString = function(data)
 {
-if (data.words == null )
-{
-	return;
-}
-else 
-{
-	var x;
-	var output = "";
-	for (x in data.words)
-	{
-		output = output + data.words[x].value + "/" + data.words[x].tag + " ";
-	}
-	return output
-	}
+  if (data.words == null )
+  {
+    return;
+  }
+  else 
+  {
+    var x;
+    var output = "";
+    for (x in data.words)
+    {
+      output = output + data.words[x].value + "/" + data.words[x].tag + " ";
+    }
+    return output
+  }
 };
