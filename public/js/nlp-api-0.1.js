97f876851ea83859464857c5f6080aea6d6d9a6a
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-07 23:58:29 -0400

    Better tokenizer

--- public/js/nlp-api-0.1.js
+++ public/js/nlp-api-0.1.js
@@ -1,10 +1,54 @@
 var nlp = {};
+
+// grep(ary[,filt]) - filters an array
+//   note: could use jQuery.grep() instead
+// @param {Array}    ary    array of members to filter
+// @param {Function} filt   function to test truthiness of member,
+//   if omitted, "function(member){ if(member) return member; }" is assumed
+// @returns {Array}  all members of ary where result of filter is truthy
+function grep(ary,filt) {
+  var result=[];
+  for(var i=0,len=ary.length;i++<len;) {
+    var member=ary[i]||'';
+    if(filt && (typeof filt === 'Function') ? filt(member) : member) {
+      result.push(member);
+    }
+  }
+  return result;
+}
+
 nlp.getParse = function(text, callback)
 {
   if (text.length > 250)
   {
+    
+    var punct='\\['+ '\\!'+ '\\"'+ '\\#'+ '\\$'+   // since javascript does not
+    '\\%'+ '\\&'+ '\\\''+ '\\('+ '\\)'+  // support POSIX character
+    '\\*'+ '\\+'+ '\\,'+ '\\\\'+ '\\-'+  // classes, we'll need our
+    '\\.'+ '\\/'+ '\\:'+ '\\;'+ '\\<'+   // own version of [:punct:]
+    '\\='+ '\\>'+ '\\?'+ '\\@'+ '\\['+
+    '\\]'+ '\\^'+ '\\_'+ '\\`'+ '\\{'+
+    '\\|'+ '\\}'+ '\\~'+ '\\]',
+
+    re=new RegExp(     // tokenizer
+      '\\s*'+            // discard possible leading whitespace
+      '('+               // start capture group
+      '\\.{3}'+            // ellipsis (must appear before punct)
+      '|'+               // alternator
+      '\\w+\\-\\w+'+       // hyphenated words (must appear before punct)
+      '|'+               // alternator
+      '\\w+\'(?:\\w+)?'+   // compound words (must appear before punct)
+      '|'+               // alternator
+      '\\w+'+              // other words
+      '|'+               // alternator
+      '['+punct+']'+        // punct
+      ')'                // end capture group
+    );
+    
+    
     // If more than 250 be really stupid and call everything a noun... 
-    var words = text.split(" ");
+    var words = grep( text.split(re) );
+    // var words = text.split(/\W+/);
     var obj = {words:[words.length]};
     for (var i = 0; i < words.length; i++) {
       obj.words[i] = {value:words[i], tag:"NN"};
